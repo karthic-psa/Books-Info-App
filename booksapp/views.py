84089245a98922c2196a2d53b025ffa1e3144ac3
Srinivasa Karthic Ponguru <karthic.psa@gmail.com>
2018-06-21 21:37:18 -0700

    feat: added top books and must reads section

--- booksapp/views.py
+++ booksapp/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 from django.views import generic
 from models import Books
 
@@ -14,6 +15,22 @@ from models import Books
 
 class IndexView(generic.ListView):
     template_name = 'index.html'
+    # context_object_name = 'random_no_list'
+    # model = Books
+    # r = random.sample(range(1,(len(Books.objects.all()))), 5)
+
+    def get_context_data(self, **kwargs):
+        r = random.sample(range(1, (len(Books.objects.all()))), 6)
+        # l = []
+        # for i in r:
+        #     l.append(Books.objects.filter(pk__in=r))
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({
+            'random_books': Books.objects.filter(pk__in=r),
+        })
+        return context
 
     def get_queryset(self):
+        r = random.sample(range(1, (len(Books.objects.all()))), 5)
         return Books.objects.all()
+
